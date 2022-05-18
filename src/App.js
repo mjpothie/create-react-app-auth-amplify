7e9a8afcf377cb65bb495815e609345c234f00a1
EC2 Default User <ec2-user@ip-10-0-1-164.ec2.internal>
2022-05-18 21:24:00 +0000

    More updates

---
+++ src/App.js
@@ -0,0 +1,84 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+import React, {useEffect, useState, Fragment} from "react";
+import Amplify, {Auth, Hub} from "aws-amplify";
+import {Container} from "react-bootstrap";
+
+import Navigation from "./components/Navigation.js";
+import FederatedSignIn from "./components/FederatedSignIn.js";
+import MainRequest from "./components/MainRequest.js";
+import "./App.css";
+
+Amplify.configure({
+  Auth: {
+    region: "us-east-1",
+    userPoolId: "us-east-1_Io92oXTkk",
+    userPoolWebClientId: "2em0ieao2je2jou8u345b9vci5",
+    oauth: {
+      domain: "https://kendrapoc.auth.us-east-1.amazoncognito.com",
+      scope: ["email", "openid", "aws.cognito.signin.user.admin", "profile"],
+      redirectSignIn: "http://localhost",
+      redirectSignOut: "http://localhost",
+      responseType: "code"
+    }
+  },
+  API: {
+    endpoints: [
+      {
+        name: "MyBlogPostAPI",
+        endpoint: "<enter here the API gateway endpoint url>"
+      }
+    ]
+  }
+});
+
+const federatedIdName =
+  "<name of the Identity Provider as configured in Cognito>";
+
+function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    Hub.listen("auth", ({payload: {event, data}}) => {
+      switch (event) {
+        case "signIn":
+        case "cognitoHostedUI":
+          setToken("grating...");
+          getToken().then(userToken => setToken(userToken.idToken.jwtToken));
+          break;
+        case "signOut":
+          setToken(null);
+          break;
+        case "signIn_failure":
+        case "cognitoHostedUI_failure":
+          console.log("Sign in failure", data);
+          break;
+        default:
+          break;
+      }
+    });
+  }, []);
+
+  function getToken() {
+    return Auth.currentSession()
+      .then(session => session)
+      .catch(err => console.log(err));
+  }
+
+  return (
+    <Fragment>
+      <Navigation token={token} />
+      <Container fluid>
+        <br />
+        {token ? (
+          <MainRequest token={token} />
+        ) : (
+          <FederatedSignIn federatedIdName={federatedIdName} />
+        )}
+      </Container>
+    </Fragment>
+  );
+}
+
+export default App;
